c14e52f4aeb74670d6b083da7d97f402a608fcdc
Fazrel <161111358+QvCNA@users.noreply.github.com>
2024-02-27 15:00:24 +0800

    Add files via upload

---
+++ words.js
@@ -0,0 +1,134 @@
+const wordList = [
+    {
+        word: "faisal",
+        hint: "yang sering dibully"
+    },
+    {
+        word: "fazrel",
+        hint: "pembuat game ini"
+    },
+    {
+        word: "novia",
+        hint: "acil-acil jual bakwan"
+    },
+    {
+        word: "zaini",
+        hint: "nama bapak reza"
+    },
+    {
+        word: "faiz",
+        hint: "yang sering bilang yatim"
+    },
+    {
+        word: "mustafa",
+        hint: "si paling ff"
+    },
+    {
+        word: "afka",
+        hint: "anak meme"
+    },
+    {
+        word: "jepri",
+        hint: "si paling osis"
+    },
+    {
+        word: "nispi",
+        hint: "novia"
+    },
+    {
+        word: "zaini",
+        hint: "penyambatan"
+    },
+    {
+        word: "ridho",
+        hint: "orangnya putih biasa di panggil riri"
+    },
+    {
+        word: "rifky",
+        hint: "cebol selain opal"
+    },
+    {
+        word: "arsyad",
+        hint: "landak kalimantan"
+    },
+    {
+        word: "matematika",
+        hint: "pelajaran yang di benci umat manusia"
+    },
+    {
+        word: "olahraga",
+        hint: "yang di tunggu laki-laki pada hari kamis"
+    },
+    {
+        word: "kantin",
+        hint: "jam kos pada kemana?"
+    },
+    {
+        word: "naufal",
+        hint: "kecil tapi seperti raja"
+    },
+    {
+        word: "abdurrahman",
+        hint: "tinggi,badak,rambut belah dua"
+    },
+    {
+        word: "faisal",
+        hint: "yang paling cool"
+    },
+    {
+        word: "iya",
+        hint: "faisal ganteng ga"
+    },
+    {
+        word: "goyangnasipadang",
+        hint: "yang di nyanyikan amang madi"
+    },
+    {
+        word: "perpus",
+        hint: "gada kerjaan"
+    },
+    {
+        word: "gibah",
+        hint: "kebiasaan cewek?"
+    },
+    {
+        word: "nispi",
+        hint: "lawak"
+    },
+    {
+        word: "bahasainggris",
+        hint: "pelajaran yang disukai developer"
+    },
+    {
+        word: "abdul",
+        hint: "nama kakak yang melajari lomba musik"
+    },
+    {
+        word: "ici",
+        hint: "nispi biasanya di panggil?"
+    },
+    {
+        word: "gatau",
+        hint: "5x5x80x200x5999x610x0x6182x017=?"
+    },
+    {
+        word: "jepri",
+        hint: "yang begandang di lomba musik siapa?"
+    },
+    {
+        word: "faiz",
+        hint: "rumahnya dibunyamin"
+    },
+    {
+        word: "sumbul",
+        hint: "nama kucing yang mati"
+    },
+    {
+        word: "latif",
+        hint: "jualan risol mayo kelas 8"
+    },
+    {
+        word: "abdurrahman",
+        hint: "pemimpin upacara"
+    }
+];
